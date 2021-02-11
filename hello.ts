c138136331b5bc7f2852b7491f809a5dd88417be
Lockett Pundt <kalifornium@hotmail.com>
2021-02-10 22:02:21 -0500

    feat(initial commit): :sparkles: initial commit

---
+++ hello.ts
@@ -0,0 +1,10 @@
+import { serve } from 'https://deno.land/std@0.86.0/http/server.ts'
+
+const server = serve({ port: 8000 })
+
+for await (const req of server) {
+  req.respond({
+    body: 'Hello Deno',
+  })
+}
+
